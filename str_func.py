be78e3c8cbba431ba2284b311dc6874579da61cd
Anastasia Pleshkova <nastya.tsvetkova@rambler.ru>
2023-12-24 09:34:39 -0500

    add str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def get_upper_text(text):
+    """
+    Выводит текст заглавными буквами
+    """
     return text.upper()
+
+def get_title_text(text):
+    """
+    Выводит каждое слово из текста с заглавной буквы
+    """
+    return text.title()
